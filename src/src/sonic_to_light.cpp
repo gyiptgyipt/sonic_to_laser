987ae3edee5fd9006f73104a593bf549eb8ab929
Htet-Wai-Yan-HWY <htetwaiyan.zyme@gmail.com>
2023-11-21 05:33:24 +0630

    angle ok

--- src/src/sonic_to_light.cpp
+++ src/src/sonic_to_light.cpp
@@ -14,7 +14,7 @@ class S2L_converter : public rclcpp::Node {
 public:
     S2L_converter(): Node("S2L_converter") {
               
-        laser_publisher_= this->create_publisher<sensor_msgs::msg::LaserScan>("/laser", 10);
+        laser_publisher_= this->create_publisher<sensor_msgs::msg::LaserScan>("/laser", 1000);
         
         sonic_subscriber = this->create_subscription<std_msgs::msg::Float32>(
             "/sonar", 10, std::bind(&S2L_converter::sensor_callback, this, std::placeholders::_1));
@@ -37,14 +37,7 @@ private:
         // std_msgs::msg::Float32 sonic;
         sensor_msgs::msg::LaserScan laser;
         
-        _Float32 cm_to_m = (_Float32)msg->data/100;
-        if (cm_to_m >4){
-            cm_to_m = 4;}
-        else if (cm_to_m == 0){
-            cm_to_m = 0.01;}
-        
-        // double distance = ("%.1f",round(cm_to_m));
-        _Float32 distance = std::ceil(cm_to_m *100.0)/100.0;
+
 
         rclcpp::Time end = this->get_clock()->now();
         // auto dt = (end-now).seconds();
@@ -53,14 +46,27 @@ private:
         laser.header.frame_id = "laser_frame";
 
         laser.angle_min = 0*M_PI/180;
-        laser.angle_max = 165*M_PI/180;
+        laser.angle_max = 90*M_PI/180;
         laser.angle_increment = M_PI/180;
         laser.time_increment = 0.001;
-        laser.scan_time = 0.001;
+        laser.scan_time = 0.01;
         laser.range_min = 0.01;
         laser.range_max = 4;
+
+        _Float32 cm_to_m = (_Float32)msg->data/100;
+        if (cm_to_m > laser.range_max){
+            cm_to_m = 4;}
+        else if (cm_to_m == 0){
+            cm_to_m = laser.range_min;}
+        
+        // double distance = ("%.1f",round(cm_to_m));
+        _Float32 distance = std::ceil(cm_to_m *100.0)/100.0;
+
+        
+        uint32_t ranges_size = std::ceil((laser.angle_max - laser.angle_min) / laser.angle_increment);
+        laser.ranges.assign(ranges_size,distance);
         laser.ranges.push_back(distance);
-        laser.ranges.resize(distance/laser.angle_increment);
+        // laser.ranges.resize(distance);
         // laser.intensities.resize(distance);
         
         laser_publisher_->publish(laser);
@@ -73,8 +79,8 @@ private:
         transform_stamped.header.frame_id ="imu_frame_bridge";  // Parent frame
         transform_stamped.child_frame_id = "laser_frame";   // Child frame
         transform_stamped.transform.translation.x = 0;  
-        transform_stamped.transform.translation.y = 0.02;
-        transform_stamped.transform.translation.z = 0.0; //msg->linear_acceleration.z;
+        transform_stamped.transform.translation.y = 0.0;
+        transform_stamped.transform.translation.z = 0.27; 
 
         tf2::Quaternion q;
         q.setRPY(0, 0, 0);
